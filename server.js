dc43e67b4eb179ca6ce8a3d7a78c4b3b05350a00
opabhijeet <abhijeet.2201164ec@iiitbh.ac.in>
2025-07-09 02:02:21 +0530

    add development flag

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(cors());
-app.use(morgan('dev'));
+process.env.DEVELOPMENT && app.use(morgan('dev'));
 app.use(express.json());
 
 app.get('/long-weekends/:countryCode', async (req, res) => {
